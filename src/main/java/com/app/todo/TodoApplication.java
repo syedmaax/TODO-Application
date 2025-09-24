6ea4093e157f361257e74f55a605fe99a7949aca
syedmaax <rehurehana85@gmail.com>
2025-09-25 00:53:52 +0530

    TODO

--- src/main/java/com/app/todo/TodoApplication.java
+++ src/main/java/com/app/todo/TodoApplication.java
@@ -3,10 +3,18 @@ package com.app.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
